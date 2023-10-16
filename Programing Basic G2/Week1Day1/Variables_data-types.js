2584e02cde6fcf0c662beee1e85726021b6b92ae
Wael Hajji <waelhajji.contact@gmail.com>
2023-10-16 16:23:28 +0100

    Week1Day1 Group1

---
+++ Programing Basic G2/Week1Day1/Variables_data-types.js
@@ -0,0 +1,58 @@
+
+
+// Variabels and Data types 
+// 0101111011110000001111
+
+
+// ==== Data types ====
+// ------ Strings -----
+
+// Collection of some charaters or numbers or etc inside  ""
+
+var name = "664646";
+var firstName = 'Wael';
+
+// what is variables ? 
+// Container 
+// Stores data or information
+var Variable = "Something";
+name = firstName;
+Variable = name ;
+// Naming Convension
+var programmingBasicsStudent="Dojo";
+// JS we use camelcase - the first is lowercase and all the rest are capital case (the first letter)
+var isThisAValidVariableName = "yes or no ";
+
+var MAN_I_REALLY_LIKE_PANCKAES_AND_TACOS ="Heck yea";  // not Valid
+
+
+var lkhdsjkgfksdhflksdhfkfjlksdffsdkf ="lol";
+
+var variableNamesDoNotNeedToHaveAnyConventionTheyCanBeOneLongWordThing = 'not good variable name';
+
+var name ;
+console.log(variableNamesDoNotNeedToHaveAnyConventionTheyCanBeOneLongWordThing) // not Good Comment
+console.log(Variable) // "666..."
+
+// ------ Number -----
+
+var pi = 3.14;
+var favNumber=7777;
+console.log(favNumber)// 7777
+
+// ------ BOOLEAN -----
+
+var vote1 =  true; // 1
+var vote2= false; // 0
+
+// ------ undefined -----
+// ¯\_(ツ)_/¯
+var fares ;
+console.log(fares) // undefined
+
+// ------ null -----
+// Data that is not undefined , but empty Typecally used for variables that are expected or waiting for data 
+
+var iheb = null ;
+iheb = 5 
+
