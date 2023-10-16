25292a0ec396f5daf29743114e701bd0b41120c1
Wael Hajji <waelhajji.contact@gmail.com>
2023-10-16 12:03:28 +0100

    Week1Day1G1

---
+++ Programming Basic/Week1Day1/variables_datataypes.js
@@ -0,0 +1,55 @@
+// Variables and data types 
+
+// what is a variable ?
+
+// Stores data or information
+// Variables Names should have some 'commn sense principle' in their names to say what the data is.
+// Container
+
+var name ="John";
+// naming Conventions
+var firstName = 'Wael';
+
+// JS we use camelCase -  the first word is lowercase and all the rest are capital case (the first letter)
+
+var programingBasicsStudent =  "Dojo";
+ 
+// ===== Data Types =====
+
+// --- Strings ---
+// Collection of characters
+var someString ="Ninja" ;
+
+
+console.log(name) // "John"
+console.log(programingBasicsStudent) // "Dojo"
+
+// --- Numbers ---
+
+var pi = 3.14;
+var favNumber = 7777;
+var seatNumber = 13;
+
+var mdfkgmldfjvlkdfjglkdfhblkdfhblkdfj= 1;
+var THIS_IS_VERY_WEIRD = "yes";
+var num1 = "1990";
+
+var num2 =1990;
+
+num2 = "BoB";
+
+// --- Undefined ---
+
+var houssem;
+// undefined
+// Variable with no assigned value
+// ¯\_(ツ)_/¯
+
+// --- Null ---
+var nulVarible = null // this commnet
+// Data that is not undefined , but empty . Typecly used for variables tha are wiating for value 
+
+// --- Booelan ---
+
+var vote1 = true // 1 "true" == 01001001
+var vote = false // 0
